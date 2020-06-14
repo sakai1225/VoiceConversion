d1b5afad9814c9073dc92b21fce3577d8206b854
堺井慶太 <54614833+sakai1225@users.noreply.github.com>
2020-06-14 10:20:54 +0900

    Add files via upload

--- StarGAN-VC2/calc.py
+++ StarGAN-VC2/calc.py
@@ -1,19 +1,30 @@
 import pyworld as pw
 import numpy as np
+import glob
+import librosa
 
 from scipy.io import wavfile
+from scipy.io.wavfile import write
 
-WAV_FILE = './StarGAN-VC2/data/speakers_test/SF1/200005.wav'
+files = glob.glob('./StarGAN-VC2/data/speakers_test/**/*.wav')
+i = 1 #ファイル名用
 
-fs, data = wavfile.read(WAV_FILE)
-data = data.astype(np.float)  # WORLDはfloat前提のコードになっているのでfloat型にしておく
+for file in files:
+    fs, data = wavfile.read(file)
+    data = data.astype(np.float)  # WORLDはfloat前提のコードになっているのでfloat型にしておく
 
-_f0, t = pw.dio(data, fs)  # 基本周波数の抽出
-f0 = pw.stonemask(data, _f0, t, fs)  # 基本周波数の修正
-ap = pw.d4c(data, f0, t, fs)  # 非周期性指標の抽出
+    _f0, t = pw.dio(data, fs)  # 基本周波数の抽出
+    f0 = pw.stonemask(data, _f0, t, fs)  # 基本周波数の修正
+    ap = pw.d4c(data, f0, t, fs)  # 非周期性指標の抽出
+    sp = pw.cheaptrick(data, f0, t, fs)  # スペクトル包絡の抽出
 
-np.save('./StarGAN-VC2/calc_f0/f0.npy',f0)
-np.save('./StarGAN-VC2/calc_ap/ap.npy', ap)
+    np.save(str('./StarGAN-VC2/calc_f0/f0_' + str(i) + '.npy'), f0)
+    np.save(str('./StarGAN-VC2/calc_ap/ap_' + str(i) + '.npy'), ap)
+    np.save(str('./StarGAN-VC2/calc_sp/sp_' + str(i) + '.npy'), sp)
+    synthesized = pw.synthesize(f0, sp, ap, fs)
 
-#sp = pw.cheaptrick(data, f0, t, fs)  # スペクトル包絡の抽出
+    librosa.output.write_wav(str('./StarGAN-VC2/synthesized/' + str(i) + '.wav'), synthesized, fs)
+    #write(str('./StarGAN-VC2/synthesized/' + str(i) + '.wav'), fs, synthesized)
+    i += 1
+
 
